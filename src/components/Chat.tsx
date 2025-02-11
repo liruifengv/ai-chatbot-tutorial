d6994ef3c9775e732fe1a8829e274d72ec00849e
liruifengv <liruifeng1024@gmail.com>
2025-02-11 13:35:58 +0800

    format

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -20,22 +20,21 @@ export default function Chat() {
 	const [attachments, setAttachments] = useState<Array<Attachment>>([])
 	return (
 		<div className="flex flex-col w-full max-w-3xl mx-auto stretch text-dark dark:text-white min-h-[calc(100vh-10rem)] gap-4 justify-between">
-      <div>
-        <div>How can I help you?</div>
-        {messages.map((m) => (
-          <div key={m.id} className="whitespace-pre-wrap">
-            {m.role === "user" ? "User: " : "AI: "}
-            {m.reasoning && (
-              <pre className="whitespace-pre-wrap">{m.reasoning}</pre>
-            )}
-            {m.content}
-          </div>
-        ))}
-      </div>
- 
+			<div>
+				<div>How can I help you?</div>
+				{messages.map((m) => (
+					<div key={m.id} className="whitespace-pre-wrap">
+						{m.role === "user" ? "User: " : "AI: "}
+						{m.reasoning && (
+							<pre className="whitespace-pre-wrap">{m.reasoning}</pre>
+						)}
+						{m.content}
+					</div>
+				))}
+			</div>
 
 			<ChatInput
-        className="mt-4 flex-1"
+				className="mt-4 flex-1"
 				input={input}
 				setInput={setInput}
 				handleSubmit={handleSubmit}
--- src/pages/api/chat.ts
+++ src/pages/api/chat.ts
@@ -1,32 +1,31 @@
 import type { APIRoute } from "astro"
 
-
 import { API_KEY } from "astro:env/server"
-import { streamText } from 'ai'
+import { streamText } from "ai"
 
-import { createOpenAICompatible } from '@ai-sdk/openai-compatible';
+import { createOpenAICompatible } from "@ai-sdk/openai-compatible"
 
 const provider = createOpenAICompatible({
-  name: 'aliyuncs',
-  apiKey: API_KEY,
+	name: "aliyuncs",
+	apiKey: API_KEY,
 	baseURL: "https://dashscope.aliyuncs.com/compatible-mode/v1",
-});
+})
 
-const model = provider('deepseek-r1');
+const model = provider("deepseek-r1")
 
 export const POST: APIRoute = async ({ request }) => {
 	const params = await request.json()
 	const { messages } = params
 
-  const result = streamText({
-    model: model,
-    messages,
-    onError({ error }) {
-      console.error(error); // your error logging logic here
-    },
-  })
+	const result = streamText({
+		model: model,
+		messages,
+		onError({ error }) {
+			console.error(error) // your error logging logic here
+		},
+	})
 
-  return result.toDataStreamResponse({
-    sendReasoning: true,
-  });
+	return result.toDataStreamResponse({
+		sendReasoning: true,
+	})
 }
